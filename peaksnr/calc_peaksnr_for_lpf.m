6fc2560bc064e151f6e61e97db424870408d211a
peterrrockii <53229013+peterrrockii@users.noreply.github.com>
2022-08-14 16:27:23 -0600

    added some more images and figured out why python was processing things weird

--- peaksnr/calc_peaksnr_for_lpf.m
+++ peaksnr/calc_peaksnr_for_lpf.m
@@ -1,15 +1,20 @@
 echo off;
 
 I = imread('lena.gif');
+% I = imread('railroad.jpg')
+% I = imread('wolfy.jpg');
 I2 = im2double(I);
-Filter = 1/9.*ones(3);
+
+
+% Filter = 1/9.*ones(3);
+Filter = [[-1 -1 -1]; [-1 9 -1]; [-1 -1 -1]] %hpf test
 NewImage = imfilter(I2, Filter, 'replicate');
 imwrite(NewImage, 'processed_lena.png');
 
 %Computing the peaksnr value assumeing the the maximum is 255 (8-bit b/w image)
 mse = 1/(256*256)*sum( (I2 - NewImage).^2, 'all')
 peaksnr = 10*log10(255^2/mse);
-[pksnr, snr] = psnr(NewImage, I2); 
+[pksnr, snr] = psnr(NewImage, I2, 255); 
 fprintf('\n The Peak-SNR value is %0.4f\n', pksnr);
 
 
@@ -21,6 +26,5 @@ imwrite(NewImage, 'processed_lena2.png');
 %Computing the peaksnr value assumeing the the maximum is 255 (8-bit b/w image)
 mse2 = 1/(256*256)*sum( (I2 - NewImage2).^2, 'all');
 peaksnr2 = 10*log10(255^2/mse2);
-[pksnr2, snr2] = psnr(NewImage2, I2);
+[pksnr2, snr2] = psnr(NewImage2, I2, 255);
 fprintf('\n The Peak-SNR value is %0.4f\n', pksnr2);
-
--- peaksnr/pycalc_peaksnr_for_lpf.py
+++ peaksnr/pycalc_peaksnr_for_lpf.py
@@ -4,27 +4,49 @@ from PIL import Image
 
 
 def main():
-    img = Image.open("lena.gif")
-    img_array = np.array(img, dtype=float)
+    img_name = "lena.gif" #small image
+    # img_name = "railroad.jpg" #medium image
+    # img_name = "wolfy.jpg" #large image
     
+    img = Image.open(img_name)
+    img_array = np.array(img, dtype=float)
 
-    dim_filter = 3
-    filter = np.full((dim_filter,dim_filter), 1/9)
-    padded_array = make_padd_array(img_array, dim_filter, 'replicate')
     
     #for BW image the maximum intesity is 255, so we need to normalize the data
-    #we normalize afterward because python is weird with floating stuff
-    peak_snr = psnr(img_array/255, do_convolution(padded_array, filter, dim_filter)/255, 255)
+    img_array = img_array/255.0
+
+    dim_filter = 3
+    # filter = np.full((dim_filter,dim_filter), 1/9)
+    filter = np.array([[-1.,-1.,-1.],[-1.,9.,-1.],[-1.,-1.,-1.]]) #hpf test
+    padded_array = make_padd_array(img_array, dim_filter, 'replicate')
+
+    
+    new_image_array = do_convolution(padded_array, filter, dim_filter)
+    new_image = Image.fromarray(new_image_array*255)
+    if new_image.mode != 'L':
+        new_image = new_image.convert('L')
+    
+    new_image_name = 'py' + img_name
+    new_image.save(new_image_name)
+    
+    peak_snr = psnr(img_array, new_image_array, 255)
     print("The peak snr value for the first filter is: ", peak_snr)
     
+    #========================= SECOND FILTER ===========================
     
     dim_filter = 5
     filter = np.full((dim_filter,dim_filter), 1/25)
     padded_array = make_padd_array(img_array, dim_filter, 'replicate')
     
-    #for BW image the maximum intesity is 255, so we need to normalize the data
-    #we normalize afterward because python is weird with floating stuff
-    peak_snr = psnr(img_array/255, do_convolution(padded_array, filter, dim_filter)/255, 255)
+    new_image_array = do_convolution(padded_array, filter, dim_filter)
+    new_image = Image.fromarray(new_image_array*255)
+    if new_image.mode != 'L':
+        new_image = new_image.convert('L')
+
+    new_image_name = '2py' + img_name
+    new_image.save(new_image_name)
+
+    peak_snr = psnr(img_array, new_image_array, 255)
     print("The peak snr value for the second filter is: ", peak_snr)
 
     return 0;
@@ -35,17 +57,18 @@ def psnr(orig_array, filtered_array, peak_val):
     peaksnr = 10*m.log(peak_val**2/mse, 10)  
     return peaksnr;
 
+
 def do_convolution(parray, filter, filter_dimension):
     rows, columns = parray.shape
     padding = m.floor(filter_dimension/2)
     rows = rows - 2*padding
     columns = columns - 2*padding
-    new_array = np.full((rows, columns), 1)
+    new_array = np.full((rows, columns), 1.0)
     
     for i in range(0,rows):
         for j in range(0, columns):
             new_array[i, j] = np.sum(parray[i:i+filter_dimension, j:j+filter_dimension]*filter)
-  
+
     return new_array;
 
 
@@ -53,9 +76,8 @@ def make_padd_array(array, filter_dimension, type):
     rows, columns = array.shape
     padding = m.floor(filter_dimension/2)
 
-    padd_array = np.full((rows + 2*padding, columns + 2*padding), 0)
+    padd_array = np.full((rows + 2*padding, columns + 2*padding), 0.0)
     padd_array[padding:-padding, padding:-padding] = array
-
 
     if type == 'replicate':
         for i in range(0,padding):
@@ -70,7 +92,7 @@ def make_padd_array(array, filter_dimension, type):
         padd_array[-padding:, -padding:] = np.full((padding, padding), array[-1,-1])
     elif type != 'zero':
         print(f"Type \"{type}\" not defined returning zero padded array")
-        
+
     return padd_array
 
 if __name__ == "__main__":
